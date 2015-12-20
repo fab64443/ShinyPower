fe410df5559aec399769cd938ce8fa2bd65aa693
Fabrice Bribsois <amerol5@yahoo.fr>
2015-12-20 21:11:42 +0100

    Add documentation

--- server.R
+++ server.R
@@ -61,5 +61,39 @@ shinyServer(
         h6('$$P\\left(\\frac{\\bar{X}-\\mu_0}{\\sigma/\\sqrt{n}}>z_{1-\\alpha};\\mu=\\mu_a\\right)$$'))
       }
     })
-
+  
+  output$documentation <- renderUI({ 
+    tags$html(
+      tags$head(tags$title('Power of a Hypothesis Test')),
+      tags$body(
+        h2('Power of a Hypothesis Test'),
+        h4('Definition'),
+        p('The probability of ',strong(em('not')),' committing a Type II error is called the ',strong('power'),
+          'of a hypothesis test.'),
+        h4('Effect Size'),
+        p('To compute the power of the test, one offers an alternative view about the "true" value of the population parameter, assuming that the null hypothesis is false. The ', strong('effect size'), ' is the difference between the true value and the value specified in the null hypothesis.'),
+        p(align="center",'Effect size = True value - Hypothesized value'),
+        p('For example, suppose the null hypothesis states that a population mean is equal to 30. A researcher might ask: What is the probability of rejecting the null hypothesis if the true population mean is equal to 32 ? In this example, the effect size would be 32-30, which equals 2.'),
+        h4('Factors That Affect Power'),
+        p('The power of a hypothesis test is affected by four factors.'),
+        tags$ul( 
+          tags$li('Sample size (n). Other things being equal, the greater the sample size, the greater the power of the test.'),
+          tags$li('The variance (sigma) of the sample. The lower the variance, the narrower the distribution and the greater the power of the test. If the distribution is narrower, the "true" value of the parameter and the value specified in the null hypothesis are more isolated.'),
+          tags$li('Significance level (alpha). The higher the significance level, the higher the power of the test. If you increase the significance level, you reduce the region of acceptance. As a result, you are more likely to reject the null hypothesis. This means you are less likely to accept the null hypothesis when it is false; i.e., less likely to make a Type II error. Hence, the power of the test is increased.'),
+          tags$li('The "true" value of the parameter being tested. The greater the difference between the "true" value of a parameter and the value specified in the null hypothesis, the greater the power of the test. That is, the greater the effect size, the greater the power of the test.')
+        )
+        
+      )
+    )  })
+  
+  
+  output$usage <- renderUI({ 
+    tags$html(
+      tags$head(tags$title('Power of a Hypothesis Test')),
+      tags$body(
+        h2('App Usage')        
+      )
+    )
+    })
+  
   })
--- ui.R
+++ ui.R
@@ -7,19 +7,29 @@ shinyUI(pageWithSidebar(
     p(strong('mu0 = 30')),
     sliderInput('mu', 'Mean',value = 32, min = 30, max = 38, step = 0.1),
     sliderInput('sigma', 'Variance',value = 4, min = 1, max = 10, step = 0.1),
-    sliderInput('n', 'Sample',value = 30, min = 10, max = 100, step = 5),
+    sliderInput('n', 'Sample size',value = 30, min = 10, max = 100, step = 5),
     sliderInput('alpha', 'alpha',value = 0.05, min = 0.01, max = 0.1, step = 0.01),
     checkboxInput("bilateral", "Bilateral", value = FALSE)
   ),
   
   mainPanel(
-    withMathJax(),
-    h2("Gaussian distributions"),
-    plotOutput('powerplot'),
-    h4('\nPower'),
-    uiOutput('formula'),
-    textOutput("power")   
-  )
+    tabsetPanel(type = "tabs", 
+                tabPanel("Documentation",
+                         htmlOutput("documentation")
+                          ),
+                tabPanel("Usage",
+                         htmlOutput("usage")
+                ),
+                tabPanel("Plot", 
+                         withMathJax(),
+                         h2("Gaussian distributions"),
+                         plotOutput('powerplot'),
+                         h4('\nPower'),
+                         uiOutput("formula"),
+                         textOutput("power")) 
+                        )
+            )
   
 ))
 
+  
